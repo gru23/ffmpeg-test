20ec7cb39709b13acb993fd9f6d92659f07be793
gru <grujica.vasic@hotmail.com>
2026-06-12 17:11:15 +0200

    adding visible time as param to toast

--- src/shared/toastHelper.ts
+++ src/shared/toastHelper.ts
@@ -3,10 +3,12 @@ import Toast from "react-native-toast-message";
 export const showToast = (
     type: 'success' | 'error' | 'info', 
     text1: string, 
-    text2?: string
+    text2?: string,
+    time?: number
 ) => {
   Toast.show({
-    visibilityTime: 4000,   // does not work
+    //visibilityTime: 4000,   // does not work
+    visibilityTime: time ?? 3000,
     type,
     text1,
     text2,
